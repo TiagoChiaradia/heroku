841436ac9d414cd349affeaa7a7172767100a6f4
TiagoChiaradia <chiaradia@ymail.com>
2022-04-07 22:36:00 -0300

    start

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+const port = process.env.PORT || 3000;  //SE NAO RECEVER PORTA, USA A 3000
+
 app.get('/', (req,res) => {
     res.render("home.ejs")
     return res.send({ message: 'Tudo OK com este método get'}); 
@@ -14,6 +16,8 @@ app.post('/', (req,res) => {
     return res.send({ message: 'Tudo OK com este método post'});
 });
 
-app.listen(3000);
+app.listen(port,()=>{
+    console.info("Aplicação Rorando")
+});
 
 module.exports = app;
